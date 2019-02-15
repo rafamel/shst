7eab77e2889cf24bdd6a5d9f13622f9cbf9e1bb1
Rafa Mel <melrcm@gmx.net>
2019-02-15 18:35:10 +0100

    chore(core, package): removes list dependency

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,5 +1,4 @@
 export * from '#/core';
-export { toArray, from as fromArray } from 'list';
 export { default as Parser } from './Parser';
 export { default as Printer } from './Printer';
 export { default as traverse } from './traverse';
